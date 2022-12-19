c29ddce81fdc0555b75d7c8da5b7606ba4aea47f
ChrysiAlexan <111639354+ChrysiAlexan@users.noreply.github.com>
2022-12-19 15:54:59 +0200

    file

---
+++ test.php
@@ -0,0 +1,37 @@
+<?php
+	
+	$fname=$_POST['fname'];
+	$lname=$_POST['lname'];
+	
+	$con=mysqli_connect("localhost","root","","database_name");
+	/*Replace database_name with the name of your database*/ 
+
+	$q=mysqli_query($con,"SELECT * FROM table_name WHERE fname='$fname' and lname='$lname'");
+	/*Replace table_name with the name of your table*/ 
+
+	if(mysqli_connect_errno()){echo "Fail".mysqli_connect_errno();}
+
+	if(mysqli_num_rows($q)>0){
+
+		$fetch=mysqli_fetch_array($q,MYSQLI_ASSOC);
+		print_r($fetch);
+
+	} else
+	{	
+		echo"Creating new user";
+
+		$q=mysqli_query($con,"INSERT into table_name(fname,lname) values('$fname','$lname')");
+		/*Replace table_name with the name of your table*/ 
+		
+	if(mysqli_connect_errno()){echo"Fail".mysqli_connect_errno();}
+	}
+
+	mysqli_close($con);
+
+	
+
+
+
+	
+
+
